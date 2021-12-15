b454e9851ca825638748ae5cabc88be3886ee034
CrazyDevCake <axel.adrian@me.com>
2021-12-15 18:10:32 +0700

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
